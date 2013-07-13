e07f6afb56717c4cb068f239b585059447ec89ca
Amrith Nayak <amrith92@gmail.com>
2013-07-13 15:11:40 +0530

    Templates didn't need to be extract()-ed

--- app/AppView.php
+++ app/AppView.php
@@ -3,20 +3,12 @@
 class AppView {
 
 	public function renderFriends($friends) {
-		if ($friends == null) {
-			$friends = false;
-		}
-
-		extract($friends);
-
 		include 'templates/header.html.php';
 		include 'templates/friends.html.php';
 		include 'templates/footer.html.php';
 	}
 
 	public function renderError($theError) {
-		extract($theError);
-
 		include 'templates/header.html.php';
 		include 'templates/error.html.php';
 		include 'templates/footer.html.php';
